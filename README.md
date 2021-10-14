dc716a585c6fd656d4c51bc11209ab4bba103c9e
Mitchell <d.r.mitchellriley@gmail.com>
2021-10-14 16:22:51 -0700

    working readme generator, and readme updated

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# readme-generator
-Generates a readme in template
+# ReadMe Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+- This project is meant to simplify the initial creation stage of a readme file. Due to every project requiring a readme, this project is meant to save time in the long run for developers.
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Questions](#questions)
+- [License](#license)
+## Installation
+- This project only requires the user to download a copy of the repo. Then make sure to run an integrated terminal of the Readme Generator.  Double check to see that you have the neccessary dependencies by running the command "npm i" in the terminal. 
+## Usage
+- After that is checked simply run the commnad "node index.js". The user should then be prompted to answer some questions about their readme file. Once the user has completed their final question, the terminal should return a message of "Generated readme", the user should then see their readme template in the holder folder.
+## Credits
+- [Inquirer](https://www.npmjs.com/package/inquirer) 
+- [Node](https://nodejs.org/en/) 
+## Questions
+- github.com/Pherenzia
+- You may reach me here at for any qeustions d.r.mitchellriley@gmail.com
+## License
+- MIT
--- index.js
+++ index.js
@@ -1,44 +1,93 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 
+
+
+
 inquirer
     .prompt([
         {
             type: 'input',
-            message: 'What is the name of your project?',
+            message: 
+'What is the name of your project?',
             name: 'title',
         },
         {
             type: 'input',
-            message: `Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-            - What was your motivation?
-            - Why did you build this project? 
-            - What problem does it solve?
-            - What did you learn? `,
+            message: 
+`Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+- What was your motivation?
+- Why did you build this project? 
+- What problem does it solve?
+- What did you learn? 
+`,
             name: 'description'
         },
         {
             type: 'input',
-            message: `What are the steps required to install your project?
-             Provide a step-by-step description of how to get the development environment running.`,
+            message: 
+`What are the steps required to install your project?
+Provide a step-by-step description of how to get the development environment running.`,
              name: 'installation'
         },
         {
             type: 'input',
-            message: `Provide instructions and examples for use. Include screenshots as needed.`,
+            message: 
+`Provide instructions and examples for use. Include screenshots as needed.`,
             name: 'instructions',
         },
         {
             type: 'input',
-            message: `List your collaborators, if any, with links to their GitHub profiles.
-            If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-            If you followed tutorials, include links to those here as well.`,
+            message: 
+`List your collaborators, if any, with links to their GitHub profiles. 
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.`,
             name: 'collabs'
         },
         {
+            type: 'input',
+            message: 'What is your github username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email'
+        },
+        {
             type: 'list',
-            message: `Choose your licensing`,
+            message: 
+`Choose your licensing`,
             choices: ['MIT', 'GPLv2', 'Apache', 'BSD 3-clause'],
             name: 'license',
         }
-    ])
+    ]).then((answers) =>{
+        console.log(answers)
+        const filename = './holder/readme.md';
+        const filecontent = 
+`# ${answers.title}
+[![License: ${answers.license}](https://img.shields.io/badge/License-${answers.license}-yellow.svg)](https://opensource.org/licenses/${answers.license})
+## Description
+- ${answers.description}
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Questions](#questions)
+- [License](#license)
+## Installation
+- ${answers.installation}
+## Usage
+- ${answers.instructions}
+## Credits
+- ${answers.collabs}
+## Questions
+- github.com/${answers.github}
+- You may reach me here at for any qeustions ${answers.email}
+## License
+- ${answers.license}
+`
+
+        fs.writeFile(filename, filecontent, (err) =>
+        err? console.error(err) : console.log('Generated readme'))
+        
+    })
